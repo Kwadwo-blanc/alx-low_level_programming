165dad6eb02a84e5c810a9902347efdd2a881794
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-07 23:45:10 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Use the puts function to print the string */
+	puts("Programming is like building a multilingual puzzle");
+
+	/* End the program with the value 0 */
+	return (0);
+}
