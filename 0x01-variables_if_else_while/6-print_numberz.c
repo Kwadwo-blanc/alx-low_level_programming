d80aedd675aa17e86f7291c64affcd3f26f37b99
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-10 07:11:06 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success/corect)
+ */
+int main(void)
+{
+int i;
+for (i = 38 ; i <= 47; i++)
+{
+putchar (i);
+}
+putchar ('\n');
+return (0);
+}
