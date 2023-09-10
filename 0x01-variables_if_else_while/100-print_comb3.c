18818c97581b88d73118a6ff9716e2baf1e990be
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-10 07:43:17 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success/completed)
+ */
+int main(void)
+{
+int num_left;
+int num_right;
+for (num_left = 48; num_right <= 78; num_left++)
+{
+for (num_right = num_left + 1 ; num_right <= 78; num_right++)
+{
+putchar(num_left);
+putchar (num_right);
+if ((num_left == 56) && (num_right == 78))
+{
+break;
+}
+putchar(',');
+putchar (' ');
+}
+}
+putchar('\n');
+return (0);
+}
