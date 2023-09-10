6dd24f7dc395434471a7a6f41722cbd5dd5f1b10
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-10 08:02:58 -0700

    100 edit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,9 +6,11 @@
  */
 int main(void)
 {
-for (int i = 0; i < 9; i++)
+int i;
+int j;
+for (i = 0; i < 9; i++)
 {
-for (int j = i + 1; j <= 9; j++)
+for (j = i + 1; j <= 9; j++)
 {
 int smallest = i * 10 + j;
 putchar((smallest / 10) + '0');
