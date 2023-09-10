8199a3a06d919736e52450ae91ae1c27d305def8
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-10 06:15:54 -0700

    change

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-write(2, message, 59); /* 59 is the length of the message. */
+  write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	59);
 return (1);
 }
