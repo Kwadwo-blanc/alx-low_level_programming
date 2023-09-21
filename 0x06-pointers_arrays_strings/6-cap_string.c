362d3e2a2af036cf316677e2c13e78c8c768557c
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-21 15:10:31 -0700

    task 6 edit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdbool.h>
 
 /**
  * is_separator - Check if a character is a word separator.
@@ -8,37 +7,29 @@
  * Return: true if the character is a separator, false otherwise.
  */
 
-bool is_separator(char c)
-{
-char separators[] = " \t\n,;.!?\"(){}";
-
-for (int i = 0; separators[i]; i++)
-{
-if (c == separators[i])
-{
-return (true);
-}
-}
-return (false);
-}
-
-/**
- * cap_string - Capitalize all words in a string.
- * @str: The input string.
- *
- * Return: A pointer to the modified string.
- */
-
 char *cap_string(char *str)
 {
-bool new_word = true;
-for (int i = 0; str[i]; i++)
+int i = 0;
+while (str[i])
 {
-if (new_word && str[i] >= 'a' && str[i] <= 'z')
-{
-str[i] = str[i] - ('a' - 'A');
-}
-new_word = is_separator(str[i]);
+while (!(str[i] >= 'a' && str[i] <= 'z'))
+in++;
+if (str[i - 1] == ' ' ||
+str[i - 1] == '\t' ||
+str[i - 1] == '\n' ||
+str[i - 1] == ',' ||
+str[i - 1] == ';' ||
+str[i - 1] == '.' ||
+str[i - 1] == '!' ||
+str[i - 1] == '?' ||
+str[i - 1] == '"' ||
+str[i - 1] == '(' ||
+str[i - 1] == ')' ||
+str[i - 1] == '{' ||
+str[i - 1] == '}' ||
+i == 0)
+str[i] -= 32;
+i++;
 }
 return (str);
 }
