0723556517c7eb62079fc2fed01d29c199d77a97
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-22 18:05:06 -0700

    task edit plenty

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -49,9 +49,12 @@ return (0);
 i = j = k = 0;
 while (i < len1 || j < len2 || carry)
 {
-int digit1 = (i < len1) ? n1[i] - '0' : 0;
-int digit2 = (j < len2) ? n2[j] - '0' : 0;
-int sum = digit1 + digit2 + carry;
+int digit1;
+int digit2;
+int sum;
+digit1 = (i < len1) ? n1[i] - '0' : 0;
+digit2 = (j < len2) ? n2[j] - '0' : 0;
+sum = digit1 + digit2 + carry;
 if (k < size_r - 1)
 {
 r[k] = (sum % 10) + '0';
