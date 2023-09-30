8d76fd412087f884a789ff702754545c074b47ce
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-29 17:29:26 -0700

    task 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,31 +13,31 @@
 
 int main(int argc, char *argv[])
 {
-int num1, i, result;
-int coins[] = {25, 10, 5, 2, 1};
-
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-num1 = atoi(argv[1]);
-result = 0;
-if (num1 < 0)
+int cents = atoi(argv[1]);
+if (cents < 0)
 {
 printf("0\n");
 return (0);
 }
 
-for (i = 0; i < 5 && num1 >= 0; i++)
-{
-while (num1 >= coins[i])
-{
-result++;
-num1 -= coins[i];
-}
-}
+int coins[] = {25, 10, 5, 2, 1};
+int coin_count = 0;
+int i = 0;
 
-printf("%d\n", result);
+while (cents > 0)
+{
+if (cents >= coins[i])
+{
+coin_count += cents / coins[i];
+cents %= coins[i];
+}
+i++;
+}
+printf("%d\n", coin_count);
 return (0);
 }
