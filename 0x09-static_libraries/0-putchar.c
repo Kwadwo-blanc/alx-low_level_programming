085bc53bcc43bd418feb813f248246c976764cac
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-29 19:24:48 -0700

    first task

--- 0x09-static_libraries/0-putchar.c
+++ 0x09-static_libraries/0-putchar.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include "main.h"
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to write
+ *
+ * Return: On success, returns the character written. On error, returns -1.
+ */
+ 
 int _putchar(char c)
+{
+return write(1, &c, 1);
+}
