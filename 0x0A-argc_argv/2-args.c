1be35c30edc180a16ac8b10bd059fc945dfe83a9
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-29 17:35:43 -0700

    task 3

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
  * main - Entry point of the program.
@@ -12,6 +11,7 @@
 int main(int argc, char *argv[])
 {
 int i;
+
 for (i = 0; i < argc; i++)
 {
 printf("%s\n", argv[i]);
