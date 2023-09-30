5bdfb9e6cf05a7ed61441098f27b7ffb61c0271e
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-09-29 18:07:42 -0700

    task 5

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,46 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /**
- * main - Entry point of the program.
- * @argc: The number of command-line arguments.
- * @argv: An array containing the command-line arguments.
- *
- * Return: 0 if successful, 1 if there are invalid arguments or no numbers.
+ * checker - checks for valid input
+ * @argc: argument count
+ * @i: counter for argv[]
+ * @l: counter for argv[][]
+ * @argv: argument vector
+ * Return: 0 on success, 1 on failure
  */
-
-int _atoi(char *s)
+int checker(int argc, int i, unsigned int l, char *argv[])
 {
-int i, c, n, len, f, digit;
-i = 0;
-c = 0;
-d = 0;
-e = 0;
-f = 0;
-digit = 0;
-
-while (s[e] != '\0')
-e++;
-while (i < e && f == 0)
-{
-if (s[i] == '-')
-++d;
-if (s[i] >= '0' && s[i] <= '9')
-{
-digit = s[i] - '0';
-if (c % 2)
-digit = -digit;
-d = d * 10 + digit;
-f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
-break;
-f = 0;
-}
-i++;
-}
-if (f == 0)
+for (i = 1; i <= argc; i++)
+for (l = 0; argv[i] != '\0' && l < strlen(argv[i]); l++)
+if (isdigit(argv[i][l]) == 0)
+return (1);
 return (0);
-return (d);
 }
 
 /**
@@ -53,16 +28,15 @@ return (d);
 
 int main(int argc, char *argv[])
 {
-int result, num1, num2;
-
-if (argc < 3 || argc > 3)
+int res, num1;
+res = 0;
+if (checker(argc, 1, 0, argv) ==1)
 {
 printf("Error\n");
 return (1);
 }
-num1 = _atoi(argv[1]);
-num2 = _atoi(argv[2]);
-result = num1 * num2;
-printf("%d\n", result);
+for (num1 = 1; num1 < argc; num1++)
+res += atoi(argv[num1]);
+printf("%d\n", res);
 return (0);
 }
