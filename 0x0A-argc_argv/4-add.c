301e080aa839d0cedd1bf4ab158439c1632292ea
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-10-01 12:57:07 -0700

    not the least

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,13 +12,18 @@
 
 int main(int argc, char *argv[])
 {
+int cents;
+int num_coins;
+int count;
+int coins[] = {25, 10, 5, 2, 1};
+
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
 
-int cents = atoi(argv[1]);
+cents = atoi(argv[1]);
 
 if (cents < 0)
 {
@@ -26,9 +31,8 @@ printf("0\n");
 return (0);
 }
 
-int coins[] = {25, 10, 5, 2, 1};
-int num_coins = sizeof(coins) / sizeof(coins[0]);
-int count = 0;
+num_coins = sizeof(coins) / sizeof(coins[0]);
+count = 0;
 int i;
 for (i = 0; i < num_coins; i++)
 {
