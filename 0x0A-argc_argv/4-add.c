621c0baa2b68fc34449b51c21c3a99e4cf0eed2e
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-10-01 12:39:34 -0700

    last one

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,45 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+#include "main.h"
 
 /**
- * checker - checks for valid input
- * @argc: argument count
- * @i: counter for argv[]
- * @l: counter for argv[][]
- * @argv: argument vector
- * Return: 0 on success, 1 on failure
- */
-
-int checker(int argc, int i, unsigned int l, char *argv[])
-{
-for (i = 1; i <= argc; i++)
-for (l = 0; argv[i][l] != '\0' && l < strlen(argv[i]); l++)
-if (isdigit(argv[i][l]) == 0)
-return (1);
-return (0);
-}
-
-/**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
  *
- * Return: 0 (Success), 1 (Error)
+ * Return: 0 if successful, 1 if there's an error
  */
 
 int main(int argc, char *argv[])
 {
-int res, num1;
-res = 0;
-if (checker(argc, 1, 0, argv) ==1)
+if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-for (num1 = 1; num1 < argc; num1++)
-res += atoi(argv[num1]);
-printf("%d\n", res);
+
+int cents = atoi(argv[1]);
+
+if (cents < 0)
+{
+printf("0\n");
+return (0);
+}
+
+int coins[] = {25, 10, 5, 2, 1};
+int num_coins = sizeof(coins) / sizeof(coins[0]);
+int count = 0;
+int i;
+for (i = 0; i < num_coins; i++)
+{
+while (cents >= coins[i])
+{
+cents -= coins[i];
+count++;
+}
+}
+
+printf("%d\n", count);
 return (0);
 }
