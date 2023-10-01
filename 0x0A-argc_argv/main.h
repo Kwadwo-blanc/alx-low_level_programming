cc8f950046a6fdff5a8a393a7ea0b2716a583bfa
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-10-01 11:17:11 -0700

    last battle

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char c);
 int _atoi(char *s);
-int main(int argc, char *argv[])
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
 
 #endif /* MAIN_H */
