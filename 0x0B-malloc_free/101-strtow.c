741790b8b253a66db39c7997e9207f656a17adbf
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-10-02 18:51:38 -0700

    task 7

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,82 +2,48 @@
 #include "main.h"
 
 /**
- * count_word - helper function to count the number of words in a string
- * @s: string to evaluate
+ * count_word - Helper function to count the number of words in a string.
+ * @s: The input string.
  *
- * Return: number of words
+ * Return: The number of words.
  */
+int count_word(char *s);
 
-int count_word(char *s)
-{
-int flag;
-int c;
-int m;
-flag = 0;
-m = 0;
-for (c = 0; s[c] != '\0'; c++)
-{
-if (s[c] == ' ')
-flag = 0;
-else if (flag == 0)
-{
-flag = 1;
-m++;
-}
-}
-return (m);
-}
 /**
- * **strtow - splits a string into words
- * @str: string to split
+ * split_words - Helper function to split a string into words.
+ * @str: The input string.
+ * @words: An array to store the words.
  *
- * Return: pointer to an array of strings (Success)
- * or NULL (Error)
+ * Return: None.
+ */
+void split_words(char *str, char **words);
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The input string.
+ *
+ * Return: A pointer to an array of strings (words) or NULL on failure.
  */
 
 char **strtow(char *str)
 {
 char **words;
-char *tmp;
-int i;
-int k;
-int len;
-int word;
-int c;
-int start;
-int end;
-k = 0;
-len = 0;
-c = 0;
-while (*(str + len))
-len++;
-word = count_word(str);
-if (word == 0)
+int word_count;
+if (str == NULL || *str == '\0')
 return (NULL);
-words = (char **) malloc(sizeof(char *) * (word + 1));
+word_count = count_word(str);
+if (word_count == 0)
+return (NULL);
+words = (char **)malloc(sizeof(char *) * (word_count + 1));
 if (words == NULL)
 return (NULL);
-for (i = 0; i <= len; i++)
-{
-if (str[i] == ' ' || str[i] == '\0')
-{
-if (c)
-{
-end = i;
-tmp = (char *) malloc(sizeof(char) * (c + 1));
-if (tmp == NULL)
-return (NULL);
-while (start < end)
-*tmp++ = str[start++];
-*tmp = '\0';
-words[k] = tmp - c;
-k++;
-c = 0;
-}
-}
-else if (c++ == 0)
-start = i;
-}
-words[k] = NULL;
+split_words(str, words);
 return (words);
 }
+int count_word(char *s)
+{
+}
+void split_words(char *str, char **words)
+{
+}
+
