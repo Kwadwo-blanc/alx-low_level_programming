43918b37c9cdb295c031f5d570b7b7f4d16e411f
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-10-03 14:34:05 -0700

    all done

---
+++ 0x09-static_libraries/5_strstr.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strstr - locates a substring
+ * @haystack: the string to search within
+ * @needle: the substring to search for
+ *
+ * Return: a pointer to the beginning of the located substring, or NULL
+ *         if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+int j;
+if (!*needle)
+return (haystack);
+for (i = 0; haystack[i]; i++)
+{
+for (j = 0; needle[j]; j++)
+{
+if (haystack[i + j] != needle[j])
+break;
+}
+if (!needle[j])
+return (haystack + i);
+}
+return (NULL);
+}
