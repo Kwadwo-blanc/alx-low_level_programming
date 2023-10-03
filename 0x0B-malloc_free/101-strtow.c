03b25667f9acbf68197cec2baa2e53d3d68a117d
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-10-02 19:18:31 -0700

    task 7

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -52,7 +52,7 @@ len++;
 words = count_word(str);
 if (words == 0)
 return (NULL);
-matrix = (char **) malloc(sizeof(char *) * (words + 1));
+matrix = ((char **) malloc(sizeof(char *) * (words + 1)));
 if (matrix == NULL)
 return (NULL);
 for (i = 0; i <= len; i++)
