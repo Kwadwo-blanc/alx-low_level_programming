6bd28a8563b77ca6d2e19c03898b8afd582209c7
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-10-09 10:31:48 -0700

    task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - prints the name of the file
+ * Description: Compiled from file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-printf("Compiled from file: %s\n", __FILE__);
+printf("%s\n", __FILE__);
 return (0);
 }
