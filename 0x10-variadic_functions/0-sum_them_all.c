083c2004e62f307dea7904d43d0cd0b6e4ba7115
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-10-12 13:49:12 -0700

    task 1

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,15 +12,14 @@ int sum_them_all(const unsigned int n, ...)
 if (n == 0)
 return (0);
 
-va_list ag;
-unsigned int sum;
+va_list args;
+unsigned int sum = 0;
 unsigned int i;
-sum = 0;
 
-va_start(ag, n);
+va_start(args, n);
 
 for (i = 0; i < n; i++)
-sum += va_arg(ag, int);
-va_end(ag);
+sum += va_arg(args, int);
+va_end(args);
 return (sum);
 }
