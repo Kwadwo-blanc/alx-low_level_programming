f2bf5a8a74c5a706b2e88190e63cd6fe9391ce67
Kwadwo-blanc <ebenezerghart@gmail.com>
2023-10-19 18:46:26 -0700

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * print_message_before_main - Prints a message before function is executed.
+ */
+void print_message_before_main(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
